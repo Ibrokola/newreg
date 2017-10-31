80e33b98e32e2154bdef12bbfb4a2124a8be8613
ibrobabs <babskolawole@gmail.com>
2017-10-31 11:42:44 -0600

    update

--- chestermere/settings/production.py
+++ chestermere/settings/production.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
+    # 'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
 
 ROOT_URLCONF = 'chestermere.urls'
